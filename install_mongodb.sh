8d3c848fcb409ae06c0025affaf941ecb4b213ea
altynbai <tauanov.aidos@gmail.com>
2020-03-26 09:49:45 +0600

    added bash -c after sudo

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0xd68fa50fea312927
+sudo bash -c apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0xd68fa50fea312927
 sudo bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list'
-sudo apt update
-sudo apt install -y mongodb-org
-sudo systemctl start mongod
-sudo systemctl enable mongod
+sudo bash -c apt update
+sudo bash -c apt install -y mongodb-org
+sudo bash -c systemctl start mongod
+sudo bash -c systemctl enable mongod
