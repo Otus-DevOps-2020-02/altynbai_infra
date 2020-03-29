225a20dafdae5d7125e57833c2c8e60ee39331e2
altynbai <tauanov.aidos@gmail.com>
2020-03-29 23:06:46 +0600

    mongodb script deleted sheban

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0xd68fa50fea312927
 echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list'
 sudo apt update
