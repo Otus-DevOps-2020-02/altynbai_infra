351df4ab44aa436a7e98e6defdbe2058b56dcbc2
altynbai <tauanov.aidos@gmail.com>
2020-03-29 22:49:25 +0600

    mongodb script edit

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-sudo bash -c apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0xd68fa50fea312927
-sudo bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list'
-sudo bash -c apt update
-sudo bash -c apt install -y mongodb-org
-sudo bash -c systemctl start mongod
-sudo bash -c systemctl enable mongod
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0xd68fa50fea312927
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list'
+apt update
+apt install -y mongodb-org
+systemctl start mongod
+systemctl enable mongod
